9b1c3928f351743d6885b77383847ffdc017affc
Duncan Boyne <duncanboyne@hotmail.co.uk>
2026-05-20 13:27:04 +0100

    Set custom domain to www.eoes.co.uk

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://eoes.co.uk',
+  site: 'https://www.eoes.co.uk',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
